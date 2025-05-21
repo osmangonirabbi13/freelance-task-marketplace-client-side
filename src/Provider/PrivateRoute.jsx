77f4ad9ecbcc0e52598b47f66135c82add2bb052
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-05-21 09:57:30 +0600

    sweetalert setup login and registion add

--- src/Provider/PrivateRoute.jsx
+++ src/Provider/PrivateRoute.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { use } from "react";
+import { AuthContext } from "./AuthProvider";
+import { Navigate, useLocation } from "react-router";
+import Loading from "../Pages/Loading";
 
 const PrivateRoute = ({ children }) => {
-  return children;
+  const { user, loading } = use(AuthContext);
+  const loction = useLocation();
+  // console.log(loction);
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (user && user?.email) {
+    return children;
+  }
+  return <Navigate state={loction.pathname} to="/auth/login"></Navigate>;
 };
 
 export default PrivateRoute;
