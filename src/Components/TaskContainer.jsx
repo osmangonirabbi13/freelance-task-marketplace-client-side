dcb6c30cec3bcffcb3000863e2251f188cc790bf
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-05-22 19:51:29 +0600

    typewriter setup

--- src/Components/TaskContainer.jsx
+++ src/Components/TaskContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLoaderData } from "react-router";
 import TaskCard from "./TaskCard";
+import { Typewriter } from "react-simple-typewriter";
 
 const TaskContainer = () => {
   const taskData = useLoaderData();
@@ -11,9 +12,34 @@ const TaskContainer = () => {
 
   return (
     <div>
-      <h1 className="text-center">Feature Services</h1>
-      <p className="text-center">
-        Discover our featured services designed to elevate your experience
+      <h1 className="text-center pt-16 text-3xl">
+        Feature{" "}
+        <span className="text-emerald-400">
+          <Typewriter
+            cursor
+            cursorBlinking
+            cursorColor="#cd0808"
+            delaySpeed={1000}
+            deleteSpeed={25}
+            loop={0}
+            typeSpeed={75}
+            words={["Services", "Task"]}
+          />
+        </span>
+      </h1>
+      <p className="text-center pt-5 text-xl">
+        <Typewriter
+          cursor
+          cursorBlinking
+          cursorColor="#cd0808"
+          delaySpeed={1000}
+          deleteSpeed={25}
+          loop={0}
+          typeSpeed={75}
+          words={[
+            "Discover our featured services designed to elevate your experience",
+          ]}
+        />
       </p>
       <div className="max-w-screen-2xl mx-auto px-8 md:px-12 lg:px-8 mt-20">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
